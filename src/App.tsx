de01d80e4452edeeedd3b469e4a50c3257a70616
camilo9090 <camilou9090@gmail.com>
2024-08-18 11:22:13 -0500

    se orgamizo el header

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ function App() {
 
   return (
     <>
-      <h1>hola Mundo</h1>
+    <header className="bg-teal-400 py-5">
+
+      <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
+    </header>
     </>
   )
 }
